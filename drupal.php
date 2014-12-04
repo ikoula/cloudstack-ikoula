3d74d262a5853f21457f197a09bbc42c66834013
Ikoula <contrib@ikoula.com>
2014-12-04 16:01:32 +0100

    Update drupal.php

--- drupal.php
+++ drupal.php
@@ -29,11 +29,11 @@ print "Port SSH publique pour cette instance :";
 $pubssh = trim(fgets(STDIN));
 
 /* UUID de l'offre de service (configuration matérielle) voulue */
-print "UUID de l'offre de service (configuration matérielle) voulue (voir FAQ ttps://support.ikoula.com/subindex-1-2-236.html): ";
+print "UUID de l'offre de service (configuration matérielle) voulue (voir FAQ https://support.ikoula.com/subindex-1-2-236.html): ";
 $serviceofferinguuid = trim (fgets(STDIN));
 
 /* UUID du modèle de systeme d'exploitation voulu */ 
-print "UUID du modèle de systeme d'exploitation voulu (voir FAQ ttps://support.ikoula.com/subindex-1-2-236.html):";
+print "UUID du modèle de systeme d'exploitation voulu (voir FAQ https://support.ikoula.com/subindex-1-2-236.html):";
 $templateuuid = trim(fgets(STDIN));
 
         // Liste complète des appels possibles : https://cloudstack.apache.org/docs/api/apidocs-4.2/TOC_User.html 
